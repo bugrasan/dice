08afeebf243ef580cd6080adf24030256fcd6280
bugrasan <>
2022-03-26 02:12:53 +0100

    statically include the wordlist, and print randomly chosen word

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,13 @@
-use std::env;
-// use std::fs;
-use std::fs::File;
-// use std::io::Read;
-use std::io;
-use std::io::BufReader;
-use std::io::BufRead;
-// use rand::prelude::*;
 use rand::Rng;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-    let file = File::open(filename)
-                    .expect("ERROR: unable to open.");
-    let reader = BufReader::new(file);
+    // statically include wordlist into program
+    static CONTENT: &'static str = include_str!("eff_large.wordlist");
 
-    let content: Vec<String> = reader.lines().filter_map(io::Result::ok).collect();
-    let len = content.len();
-
+    // get a random line
+    let len: usize = CONTENT.lines().count();
     let dice = rand::thread_rng().gen_range(0..len);
-    println!("{}", content[dice]);
+
+    // finally print the word
+    println!("{}", CONTENT.lines().nth(dice).unwrap_or(""));
 }
